fa1c8b3f369de9aeeac98865a61932075efd868e
Dmitriygame <petrosov.dimas@mail.ru>
2020-03-09 20:53:10 +0300

    End v.3

---
+++ integral/integral/class_integral_calculator.cpp
@@ -0,0 +1,6 @@
+#include "class_integral_calculator.h"
+
+integralCalculator::integralCalculator()
+{
+
+}
---
+++ integral/integral/class_integral_calculator.h
@@ -0,0 +1,11 @@
+#ifndef CLASS_INTEGRAL_CALCULATOR_H
+#define CLASS_INTEGRAL_CALCULATOR_H
+
+
+class integralCalculator
+{
+public:
+    integralCalculator();
+};
+
+#endif // CLASS_INTEGRAL_CALCULATOR_H
