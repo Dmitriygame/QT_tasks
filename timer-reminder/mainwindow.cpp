34e2800986a78c27b9f983a883b933f684385f6f
Dmitriygame <petrosov.dimas@mail.ru>
2020-03-11 20:38:19 +0300

    create tray and timer

--- timer-reminder/mainwindow.cpp
+++ timer-reminder/mainwindow.cpp
@@ -6,9 +6,60 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Remainder");
+
+    trayIcon = new QSystemTrayIcon(this);
+    trayIcon->setIcon(this->style()->standardIcon(QStyle::SP_ComputerIcon));
+    trayIcon->setToolTip("Remainder");
+
+    QMenu * menu = new QMenu(this);
+    QAction * viewWindow = new QAction(trUtf8("Open window"), this);
+    QAction * quitAction = new QAction(trUtf8("Exit"), this);
+
+    connect(viewWindow, SIGNAL(triggered()), this, SLOT(show()));
+    connect(quitAction, SIGNAL(triggered()), this, SLOT(close()));
+
+    menu->addAction(viewWindow);
+    menu->addAction(quitAction);
+
+    trayIcon->setContextMenu(menu);
+    trayIcon->show();
+
+    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+            this, SLOT(iconActivated(QSystemTrayIcon::ActivationReason)));
+
+
+    timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(slotTimerAlarm()));
+    timer->start(period_minutes * 60000);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::closeEvent(QCloseEvent * event) {
+    if (ui -> checkBox -> isChecked()) {
+        event -> ignore();
+    }
+    this->hide();
+}
+
+void MainWindow::iconActivated(QSystemTrayIcon::ActivationReason reason) {
+    switch (reason){
+    case QSystemTrayIcon::Trigger:
+        if(!this->isVisible()) {
+            this->show();
+        } else {
+            this->hide();
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void MainWindow::slotTimerAlarm() {
+    iconActivated(QSystemTrayIcon::Trigger);
 }
--- timer-reminder/mainwindow.h
+++ timer-reminder/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFont>
+#include <QTimer>
+
+#include <QCloseEvent>
+#include <QSystemTrayIcon>
+#include <QAction>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +21,20 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void closeEvent(QCloseEvent * event);
+
+private slots:
+    void slotTimerAlarm();
+    void iconActivated(QSystemTrayIcon::ActivationReason reason);
+
 private:
     Ui::MainWindow *ui;
+    QTimer *timer;
+    QSystemTrayIcon         * trayIcon;
+
+    ///
+    int period_minutes = 1;
 };
 
 #endif // MAINWINDOW_H
